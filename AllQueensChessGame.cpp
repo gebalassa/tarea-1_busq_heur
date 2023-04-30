20e728074ee0a8f5f00c850afd64c396852fe4dc
gebalassa <luciuswrath@hotmail.com>
2023-04-30 18:48:30 -0400

    input_move(): Parseo inicial input

--- AllQueensChessGame.cpp
+++ AllQueensChessGame.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <map>
 #include <time.h>
+#include <iomanip>
+#include <limits>
 #include "Bitboard.cpp"
 
 using namespace std;
@@ -43,27 +45,87 @@ namespace AllQueensChess {
 				cout << "Turno inicial: " << number_to_team[turn] << "\n";
 
 				// Input movimiento
-
-
+				input_move();
 
 
 				exit = true;
 			}
 		}
 
+		// Realizar ciclo de un input de movimiento
 		void input_move(){
 			
 			bool valid_piece = false;
 			bool valid_move = false;
+			int piece_fil;
+			int piece_col;
 			while (!valid_move && !exit){
 				// Elección de pieza
 				while (!valid_piece && !exit){
 					char raw[4] = "EEE";
+					piece_fil = -1;
+					piece_col = -1;
+					int parsed = -1;
+					// Instrucción
 					cout << "Ingrese pieza (fil-col): ";
-					// Parsing
-					cin >> raw;
-					cout << raw;
-					exit = true;
+
+					// PARSING
+					// **POR TEMAS DE TIEMPO SUPONDREMOS ALGUN GRADO DE RESTRICCION DEL USUARIO Y SUS INPUTS**
+					// Limita input a 3 chars+terminador. Ignora el resto hasta newline.
+					cin >> std::setw(4) >> raw; cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					// DEBUG: Imprime esto si falla en parsear a chars
+					if (cin.fail()){
+						cout << "FAILED" <<endl;
+					}
+					// Chequeo símbolos correctos
+					// Chequeo salida
+					if (raw[0] == 'q'){ exit = true; break; }
+					// Chequeo caracter fila
+					parsed = raw[0] - '0'; // Char numérico->Valor int real
+					switch(parsed){
+						case 0:
+							piece_fil = 0; break;
+						case 1:
+							piece_fil = 1; break;
+						case 2:
+							piece_fil = 2; break;
+						case 3:
+							piece_fil = 3; break;
+						case 4:
+							piece_fil = 4; break;
+						default:
+							cout << "Fila invalida" << endl; break;
+					}
+					if (piece_fil == -1) { continue; }
+					
+					// Chequeo caracter guión central "-"
+					if (raw[1] != '-') { cout << "Separador fil-col invalido." << endl; continue; }
+					
+					// Chequeo caracter columna
+					parsed = raw[2] - '0'; // Char numérico->Valor int real
+					switch(parsed){
+						case 0:
+							piece_col = 0; break;
+						case 1:
+							piece_col = 1; break;
+						case 2:
+							piece_col = 2; break;
+						case 3:
+							piece_col = 3; break;
+						case 4:
+							piece_col = 4; break;
+						default:
+							cout << "Columna invalida" << endl; break;
+					}
+					if (piece_col == -1) { continue; }
+					
+					//----DEBUG-------
+					// cout << "Fila: " << piece_fil << " Columna: "<< piece_col << endl;
+					// cin.get();
+					//----------------
+					
+					
+					//exit = true;
 				}
 			}
 		}
